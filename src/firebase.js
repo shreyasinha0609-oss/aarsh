9e6fc130758af5e0c0fb0c7e56ba4fa7d39d6be3
shreya <shreyasinha0609@gmail.com>
2026-09-03 19:22:07 +0530

    firebase config fixed

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TERA_API_KEY",
-  authDomain: "TERA_AUTH_DOMAIN",
-  projectId: "TERA_PROJECT_ID",
-  storageBucket: "TERA_BUCKET",
-  messagingSenderId: "TERA_SENDER_ID",
-  appId: "TERA_APP_ID"
+  apiKey: "AIzaSyDJ9C0J4Lw8BBBoLeahdgLJpid0-ra5eBc",
+  authDomain: "aarsh-live.firebaseapp.com",
+  projectId: "aarsh-live",
+  storageBucket: "aarsh-live.firebasestorage.app",
+  messagingSenderId: "1097084173712",
+  appId: "1:1097084173712:web:d7d5366f92b0b9483c0715",
+  measurementId: "G-CBD3D7ZF06"
 };
 
 const app = initializeApp(firebaseConfig);
